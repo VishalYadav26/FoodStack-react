f239500bd1e854a5fb97589e84f4711433e65f14
Vishal <vishalyadav26092002@gmail.com>
2022-12-20 12:26:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,130 @@
+import React from "react";
+import Resturant from "./component/Basics/Resturant";
+// // import UseState from "./component/Hooks/useState";
+// // import UseEffect from "./component/Hooks/useEffect";
+// import UseReducer from "./component/Hooks/useReducer";
+
+const App = () => {
+  return <Resturant />;
+};
+
+export default App;
+
+
+// import React from 'react'
+// import Restaurant from "./component/Basics/Restaurant";
+
+// const App = () => {
+//   return <Restaurant />
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div>
+//       <MyName />
+//       <h1 className="myclassname"> Welcome to Vishal's World</h1>
+//       <p>Awesome</p>
+//     </div>
+//   )
+// }
+// const MyName =() => {
+//   return <h1>Vishal Suryanath Yadav</h1>
+// }
+
+// // function App() {
+// //   return <h1>Hello World</h1>
+// // }
+
+// export default App
+
+
+
+
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,183 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/maggi.jpg",
+      name: "maggi",
+      category: "breakfast",
+      price: "12₹",
+      description:
+        "Maggi 2-Minutes Noodles have been a classic Indian snack for a good few decades now. Nestle brings you another delicious instant food product - Maggi 2-Minute Masala Instant Noodles! These Maggi noodles offer you the delicious masala flavour that will leave you wanting for more. It is not just loved by young ones but adults too.  ",
+    },
+  
+    {
+      id: 2,
+      image: "images/allupakoida.jpg",
+      name: "allu pakoda",
+      category: "evening",
+      price: "20₹",
+      description:
+        "Pakoda is a spiced fritter originating from the Indian subcontinent. They are sold by street vendors and served in restaurants in South Asia and UK. It consists of items, often vegetables such as potatoes and onions, coated in seasoned gram flour batter and deep fried.The pakora is known also under other spellings including pikora, pakoda, pakodi ",
+    },
+    {
+      id: 3,
+      image: "images/corn.jpg",
+      name: "corn",
+      category: "breakfast",
+      price: "10₹",
+      description:
+        "Sweet corn also called sugar corn and pole corn, is a variety of maize grown for human consumption with a high sugar content. Sweet corn is the result of a naturally occurring recessive mutation in the genes which control conversion of sugar to starch inside the endosperm of the corn kernel. Sweet corn is picked when still in the immature and prepared and eaten as a vegetable, rather than field corn.",
+    },
+    {
+      id: 4,
+      image: "../images/chola.jpg",
+      name: "chhola",
+      category: "lunch",
+      price: "50₹",
+      description:
+        "Chhole is a Punjabi word for chickpeas and masala is a word for spices. So this dish is nothing but chickpeas cooked to perfection with various spices, onions, tomatoes and herbs. Chickpea is a legume that is relished all over India.",
+    },
+    {
+      id: 5,
+      image: "../images/pizza.jpg",
+      name: "pizza",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Deceptively simple, the Margherita pizza is made with basil, fresh mozzarella, and tomatoes. There's a reason it's an Italian staple and one of the most popular types of pizza in the country.",
+    },
+    {
+      id: 6,
+      image: "../images/nonvegthali.jpg",
+      name: "Non-Veg Thali",
+      category: "dinner",
+      price: "180₹",
+      description:
+        "If you're a chicken-lover, consider a chicken Thali plate. The platter can include Jeera rice, naan bread, Kaali Daal, butter chicken, chicken Saagwala, Dahi Baingan, pickle, and salad. And Jalebi would make a great dessert for this platter.",
+    },
+    {
+      id: 7,
+      image: "../images/sweet.jpg",
+      name: "Sweets",
+      category: "dinner",
+      price: "60₹",
+      description:
+        "Sweets are made with some combination of flour, sugar, nuts, legumes, and milk or khoya (a semi-solid dairy product made by slowly boiling milk until it thickens), and then often enhanced with cardamom, rose water, or saffron. ",
+    },
+    {
+      id: 8,
+      image: "../images/rajmarice.jpg",
+      name: "Rajma Rice",
+      category: "lunch",
+      price: "60₹",
+      description:
+        "Rajma Rice can be one of the best weight-loss foods to include in your diet plan, especially for vegetarians. Even Shilpa Shetty Kundra had once revealed the realisation that she didn't need “asparagus and quinoa to lose weight. Rajma chawal is kidney beans served with boiled rice.",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "samosa",
+      category: "evening",
+      price: "10₹",
+      description:
+        "The traditional Indian Samosa is large and its filling is mainly made up of potatoes and peas. You can even add chopped cashew nuts and raisins to make them a rich and tasty snack. you can also add some anardaana or black salt to the filling mixture. ",
+    },
+  ];
+  
+  export default Menu;
+
+
+
+// const Menu =[
+//     {
+//     id:1,
+//     image: "images/maggi.jpg",
+//     name: "maggi",
+//     category: "breakfast",
+//     price: "12₹",
+//     description:
+//      "I love Maggi realy oo yues lorem Condimentum Lorem ipsum dolor sit amet consectetur. ",
+// },
+
+
+//     {
+//     id:2,
+//     image: "images/pakoida.jpg",
+//     name: "allu pakoida",
+//     category: "evening",
+//     price: "20₹",
+//     description:
+//      "I love allu pakoida realy oo yues lorem Condimentum Lorem ipsum dolor sit amet consectetur. ",
+// },
+
+//     {
+//     id:3,
+//     image: "images/corn.jpg",
+//     name: "corn",
+//     category: "breakfast",
+//     price: "12₹",
+//     description:
+//      "I love Maggi realy oo yues lorem Condimentum Lorem ipsum dolor sit amet consectetur. ",
+// },
+
+//     {
+//     id:1,
+//     image: "images/maggi.jpg",
+//     name: "maggi",
+//     category: "breakfast",
+//     price: "12₹",
+//     description:
+//      "I love Maggi realy oo yues lorem Condimentum Lorem ipsum dolor sit amet consectetur. ",
+// },
+
+// {
+//     id:1,
+//     image: "images/maggi.jpg",
+//     name: "maggi",
+//     category: "breakfast",
+//     price: "12₹",
+//     description:
+//      "I love Maggi realy oo yues lorem Condimentum Lorem ipsum dolor sit amet consectetur. ",
+// },
+
+// {
+//     id:1,
+//     image: "images/maggi.jpg",
+//     name: "maggi",
+//     category: "breakfast",
+//     price: "12₹",
+//     description:
+//      "I love Maggi realy oo yues lorem Condimentum Lorem ipsum dolor sit amet consectetur. ",
+// },
+
+// {
+//     id:1,
+//     image: "images/maggi.jpg",
+//     name: "maggi",
+//     category: "breakfast",
+//     price: "12₹",
+//     description:
+//      "I love Maggi realy oo yues lorem Condimentum Lorem ipsum dolor sit amet consectetur. ",
+// },
+
+// {
+//     id:1,
+//     image: "images/maggi.jpg",
+//     name: "maggi",
+//     category: "breakfast",
+//     price: "12₹",
+//     description:
+//      "I love Maggi realy oo yues lorem Condimentum Lorem ipsum dolor sit amet consectetur. ",
+// },
+
+// {
+//     id:1,
+//     image: "images/maggi.jpg",
+//     name: "maggi",
+//     category: "breakfast",
+//     price: "12₹",
+//     description:
+//      "I love Maggi realy oo yues lorem Condimentum Lorem ipsum dolor sit amet consectetur. ",
+// },
+// ]
+
+// export default Menu;
